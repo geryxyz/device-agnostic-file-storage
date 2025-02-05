f24da33402e8bd8a5ebf44ec749d8fe498e40c3e
Gergő Balogh <geryxyz@gmail.com>
2025-02-05 21:32:06 +0100

    feat(remarkable): assembling the metadatas into a graph, but the trees has to be marked somehow

--- src/akindness/device_agnostic_file_storage/remarkable/explore_document_tree.py
+++ src/akindness/device_agnostic_file_storage/remarkable/explore_document_tree.py
@@ -1,6 +1,8 @@
+import json
 import shutil
 from pathlib import Path
 
+from networkx import DiGraph
 from paramiko.client import SSHClient
 
 from akindness.device_agnostic_file_storage.remarkable.authentication import get_remarkable_password, \
@@ -11,6 +13,11 @@ REMARKABLE_DOCUMENTS_PATH = "/home/root/.local/share/remarkable/xochitl/"
 
 
 def explore(temp_dir: Path = Path.cwd() / "temp"):
+    download_metadata(temp_dir)
+    graph: DiGraph = assemble_file_system_graph(temp_dir)
+
+
+def download_metadata(temp_dir):
     client = SSHClient()
     client.load_system_host_keys()
     client.connect(
@@ -19,10 +26,8 @@ def explore(temp_dir: Path = Path.cwd() / "temp"):
         password=get_remarkable_password(),
 
     )
-
     sftp = client.open_sftp()
     metadata_path_collection = _get_metadata_path_collection(sftp)
-
     if temp_dir.exists():
         shutil.rmtree(temp_dir)
     temp_dir.mkdir(parents=True)
@@ -39,3 +44,24 @@ def _get_metadata_path_collection(sftp) -> tuple[str, ...]:
         if raw_document_path.endswith(".metadata")
     ]
     return tuple(metadata_path_collection)
+
+
+def assemble_file_system_graph(temp_dir: Path) -> DiGraph:
+    graph = DiGraph()
+    for metadata_path in temp_dir.iterdir():
+        if not metadata_path.is_file() or not (metadata_path.suffix == ".metadata"):
+            continue
+        current_id = metadata_path.stem
+        graph.add_node(current_id)
+    for metadata_path in temp_dir.iterdir():
+        if not metadata_path.is_file() or not (metadata_path.suffix == ".metadata"):
+            continue
+        with open(metadata_path, "r", encoding="utf-8") as f:
+            metadata = json.load(f)
+            current_id = metadata_path.stem
+            metadata["id"] = current_id
+            graph.add_node(current_id, **metadata)
+            parent_id: str = metadata.get("parent")
+            if parent_id.strip() != "":
+                graph.add_edge(parent_id, current_id)
+    return graph
--- src/akindness/device_agnostic_file_storage/remarkable/test_explore_document_tree.py
+++ src/akindness/device_agnostic_file_storage/remarkable/test_explore_document_tree.py
@@ -1,5 +1,14 @@
-from akindness.device_agnostic_file_storage.remarkable.explore_document_tree import explore
+from pathlib import Path
+
+import networkx
+
+from akindness.device_agnostic_file_storage.remarkable.explore_document_tree import explore, assemble_file_system_graph
 
 
 def test_explore_document_tree():
     explore()
+
+
+def test_assemble_file_system_graph():
+    graph = assemble_file_system_graph(Path.cwd() / "temp")
+    networkx.write_graphml(graph, "graph.graphml")
